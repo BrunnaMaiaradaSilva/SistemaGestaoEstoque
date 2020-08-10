3120d0562bdd2bbc9ae2bea2f87d3905d3cb82e6
Brunna Silva <silva.brunnam@gmail.com>
2020-08-09 23:03:26 -0300

    Adição de classe genérica

---
+++ MyInventory/src/Pessoa.java
@@ -0,0 +1,69 @@
+public class Pessoa {
+
+	private String nome;
+	private String sobrenome;
+	private String cpf;
+	private String cnpj;
+	private String rg;
+	private String Endereco;
+	protected String email;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getEndereco() {
+		return Endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public String setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+		return cnpj;
+	}
+	
+}
