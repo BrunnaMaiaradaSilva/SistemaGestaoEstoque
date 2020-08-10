4c02aa59d026397ef2d774acf7c9f62557fe0d7f
Brunna Silva <silva.brunnam@gmail.com>
2020-08-09 23:02:45 -0300

    alteração dos atributos da classe vendedor e adição do método de autenticação

--- MyInventory/src/Vendedor.java
+++ MyInventory/src/Vendedor.java
@@ -1,60 +1,14 @@
 
-public class Vendedor {
-	private String nome;
-	private String sobrenome;
-	private String email;
-	private String cpf;
-	private String rg;
-	private String Endereco;
+public class Vendedor extends Pessoa {
 	
+	private int senha;
 	
-	public String getNome() {
-		return nome;
+	public boolean autentica(int senha, String email) {
+		if(this.senha == senha && this.email == this.email) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getSobrenome() {
-		return sobrenome;
-	}
-	
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	public String getCpf() {
-		return cpf;
-	}
-	
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
-	public String getRg() {
-		return rg;
-	}
-	
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
-	
-	public String getEndereco() {
-		return Endereco;
-	}
-	
-	public void setEndereco(String endereco) {
-		Endereco = endereco;
-	}
-	
-	
+		
 }
