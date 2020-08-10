d6657dc3bb1e1d700b9f0ecbcb11bdf9f87bde27
Brunna Silva <silva.brunnam@gmail.com>
2020-08-09 23:01:44 -0300

    alteração dos campos da classe cliente

--- MyInventory/src/Cliente.java
+++ MyInventory/src/Cliente.java
@@ -1,49 +1,6 @@
 
-public class Cliente {
+public class Cliente extends Pessoa {
 
-	private String cpfOuCnpj;
 	private String telefone;
-	private String nome;
-	private String email;
-	private String endereço;
 
-	public String getCpfOuCnpj() {
-		return cpfOuCnpj;
-	}
-
-	public void setCpfOuCnpj(String cpfOuCnpj) {
-		this.cpfOuCnpj = cpfOuCnpj;
-	}
-
-	public String getTelefone() {
-		return telefone;
-	}
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getEndereço() {
-		return endereço;
-	}
-
-	public void setEndereço(String endereço) {
-		this.endereço = endereço;
-	}
 }
