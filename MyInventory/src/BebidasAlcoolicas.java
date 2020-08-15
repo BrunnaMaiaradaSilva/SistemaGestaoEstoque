c37a0e3fa1c0fc848ffd7e40cc4d7dcdbaf3362f
Brunna Silva <silva.brunnam@gmail.com>
2020-08-14 21:04:24 -0300

    Bebidas Alcoolicas

---
+++ MyInventory/src/BebidasAlcoolicas.java
@@ -0,0 +1,49 @@
+
+public class BebidasAlcoolicas {
+	private String cerveja;
+	private String vodka;
+	private String vinho;
+	private String cachaca;
+	private String hidromel;
+
+	public String getCerveja() {
+		return cerveja;
+	}
+
+	public void setCerveja(String cerveja) {
+		this.cerveja = cerveja;
+	}
+
+	public String getVodka() {
+		return vodka;
+	}
+
+	public void setVodka(String vodka) {
+		this.vodka = vodka;
+	}
+
+	public String getVinho() {
+		return vinho;
+	}
+
+	public void setVinho(String vinho) {
+		this.vinho = vinho;
+	}
+
+	public String getCachaca() {
+		return cachaca;
+	}
+
+	public void setCachaca(String cachaca) {
+		this.cachaca = cachaca;
+	}
+
+	public String getHidromel() {
+		return hidromel;
+	}
+
+	public void setHidromel(String hidromel) {
+		this.hidromel = hidromel;
+	}
+
+}
